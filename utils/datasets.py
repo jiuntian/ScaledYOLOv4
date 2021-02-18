190744d34f1e25bad01eaf64ac51bbab9600e30d
Hoe Jiun Tian <jiuntian@gmail.com>
2021-02-18 19:16:08 +0800

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -378,7 +378,10 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
             if l.shape[0]:
                 assert l.shape[1] == 5, '> 5 label columns: %s' % file
                 assert (l >= 0).all(), 'negative labels: %s' % file
-                assert (l[:, 1:] <= 1).all(), 'non-normalized or out of bounds coordinate labels: %s' % file
+                if l[:, 1:] <= 1).all():
+                    print('non-normalized or out of bounds coordinate labels: %s, skip' % file
+                    continue
+#                 assert (l[:, 1:] <= 1).all(), 'non-normalized or out of bounds coordinate labels: %s' % file
                 if np.unique(l, axis=0).shape[0] < l.shape[0]:  # duplicate rows
                     nd += 1  # print('WARNING: duplicate rows in %s' % self.label_files[i])  # duplicate rows
                 if single_cls:
